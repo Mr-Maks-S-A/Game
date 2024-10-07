255a6954b2572346161819f76add0f4e4fb87f7a
Maksim <108225119+Mr-Maks-S-A@users.noreply.github.com>
2024-10-07 11:54:51 +0300

    Adding an external SDL3 library to the project. Both in the repository and in the Cmake file using content fetching.

--- 00 Src/main.cpp
+++ 00 Src/main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
 #include <config.hpp>
+#include <SDL3/SDL.h>
+
 
 auto main (int argc ,char* argv[])-> int {
 
-    std::cout<<PROGECT_VERSION
-    <<"\nFor Start";
+    std::cout<<PROGECT_VERSION;
+    
+    (void)argc;
+    (void)argv;
 
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed (%s)", SDL_GetError());
+        return 1;
+    }
 
-    return EXIT_SUCCESS;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if (!SDL_CreateWindowAndRenderer("SDL issue", 640, 480, 0, &window, &renderer)) {
+        SDL_Log("SDL_CreateWindowAndRenderer failed (%s)", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    while (true) {
+        int finished = 0;
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                finished = 1;
+                break;
+            }
+        }
+        if (finished) {
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+     return EXIT_SUCCESS;
 };
